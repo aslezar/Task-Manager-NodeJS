12696a0b42c8f637da23af2a6c2228a21c2f2eaa
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-04-11 03:07:01 +0530

    11Apr2023

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 const { CustomAPIError } = require('../errors/coustom-error')
 const errorHandlerMiddleware = (err, req, res, next) => {
     if (err instanceof CustomAPIError)
-        return res.status(err.statusCode).json({ msg: err.msg });
+        return res.status(err.statusCode).json({ message: err.message });
     return res.status(500).json({ msg: err });
 }
 
